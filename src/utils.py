343ecbc17b6aeb674d54ce88fb2a3fb1dd946f26
Arumugam K <aruashwin04@gmail.com>
2023-12-03 15:43:27 +0530

    src  files are updated

---
+++ src/utils.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+import numpy as np 
+import pandas as pd
+import joblib
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        joblib.dump(obj, file_path)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param,class_weights):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            # Handle class weights for specific models
+            if isinstance(model, RandomForestClassifier):
+                class_weights = {0: 0.7184147, 1: 1.64461161}
+                model.set_params(class_weight=class_weights)
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Example: Using accuracy for classification
+            train_model_score = accuracy_score(y_train, y_train_pred)
+            test_model_score = accuracy_score(y_test, y_test_pred)
+
+            # train_model_score = precision_score(y_train, y_train_pred)
+            # test_model_score = precision_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        return joblib.load(file_path)
+
+    except Exception as e:
+        raise CustomException(e, sys)
